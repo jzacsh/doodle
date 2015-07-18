25f2b2a8736f044c042f46110d6a1b97692de717
Jonathan Zacsh <jzacsh@gmail.com>
2015-07-18 19:49:59 -0400

    mostly fix issue #6: get main event (touch) working on desktop

--- src/lib/angular.js
+++ src/lib/angular.js
@@ -1,4 +1,7 @@
 'use strict';
 // TODO: wtf?
 require('angular');
+
+// TODO ... hack because i need to load touchemulator *somewhere*
+require('hammer-touchemulator')();
 module.exports = window.angular;
